376bfd1e9280db992e776b5ae2f8de9cbc44c352
mmAbdullahrana <ar292211@gmail.com>
2025-06-28 18:44:30 +0500

    Working on authentication and authorization

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
+const bcrypt = require('bcryptjs');
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -15,19 +17,41 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please provide a valid email address'],
     lowercase: true
   },
-  photo: {
-    type: String
-  },
+  photo: String,
   password: {
     type: String,
     required: [true, 'Password is required'],
-    minLength: [8, 'Password must be at least 8 characters long']
+    minLength: [8, 'Password must be at least 8 characters long'],
+    select: false
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password']
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator(el) {
+        return el === this.password;
+      },
+      message: 'Passwords do not match'
+    }
   }
 });
+
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined; // Remove passwordConfirm after hashing
+  next();
+});
+
+userSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
